de404695ede0d841ad8375e93fd73aff96f924f3
mWalrus <hellkvistoskar@protonmail.com>
2022-12-25 19:48:26 +0100

    feat: adding flameshot bind

--- config.h
+++ config.h
@@ -57,15 +57,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *browsercmd[]  = { "firefox", NULL };
-static const char *discordcmd[]  = { "discord", NULL };
+static const char *dmenucmd[] 			= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *termcmd[]  			= { "alacritty", NULL };
+static const char *browsercmd[]  		= { "firefox", NULL };
+static const char *discordcmd[]  		= { "discord", NULL };
+static const char *flameshotcmd[]  	= { "flameshot", "gui", NULL};
 
 #include "exitdwm.c"
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
+	{ MODKEY, 											XK_Print,	 spawn,					 {.v = flameshotcmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY, 											XK_b,      spawn,      		 {.v = browsercmd } },
