c4e098d0f37e467f5f073196409e97ac2f41ff19
mWalrus <hellkvistoskar@protonmail.com>
2022-12-25 22:01:32 +0100

    feat: applying colorbar patch and catppucchin colors

--- config.h
+++ config.h
@@ -7,15 +7,21 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_black[]				= "#000000";
+static const char col_gray1[]       = "#232634";
+static const char col_gray2[]       = "#414559";
+static const char col_gray3[]       = "#b5bfe2";
+static const char col_gray4[]       = "#c6d0f5";
+static const char col_mauve[]        = "#ca9ee6";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_mauve,  col_mauve  },
+	[SchemeStatus]  = { col_gray3, col_gray1,  col_black  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  = { col_black, col_mauve, col_black  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  = { col_gray3, col_gray1,  col_black  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  = { col_gray4, col_gray1,  col_black  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  = { col_gray3, col_gray1,  col_black  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
@@ -57,7 +63,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] 			= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] 			= { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_mauve, "-sf", col_gray4, NULL };
 static const char *termcmd[]  			= { "alacritty", NULL };
 static const char *browsercmd[]  		= { "firefox", NULL };
 static const char *discordcmd[]  		= { "discord", NULL };
