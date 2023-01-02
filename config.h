d4964ca7d663860c297e9ee87be83d0eca7941a1
mWalrus <hellkvistoskar@protonmail.com>
2023-01-02 01:12:40 +0100

    fix: set snap pixel distance from 34 -> 10

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *colors[][3]      = {
