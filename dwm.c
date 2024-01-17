49405940e648829268dae9ee50a60ae69f69cc8a
mWalrus <hellkvistoskar@protonmail.com>
2024-01-17 10:54:00 +0100

    feat: apply bakkeby's steam patch
    This improves handling of fullscreen steam games in dwm.
    
    The patch file is added to the ./patches directory
    and you can find the source here:
    https://raw.githubusercontent.com/bakkeby/patches/master/dwm/dwm-steam-6.3.diff

--- dwm.c
+++ dwm.c
@@ -94,6 +94,10 @@ struct Client {
 	int bw, oldbw;
 	unsigned int tags;
 	int isfixed, isfloating, isurgent, neverfocus, oldstate, isfullscreen;
+	// FIXME: moving the cursor between windows moves the cursor and the focus back
+	//        this is most likely due to focus handling when it comes to steam apps.
+	//        Investigate...
+	int issteam;
 	Client *next;
 	Client *snext;
 	Monitor *mon;
@@ -330,6 +334,9 @@ applyrules(Client *c)
 	XGetClassHint(dpy, c->win, &ch);
 	class    = ch.res_class ? ch.res_class : broken;
 	instance = ch.res_name  ? ch.res_name  : broken;
+
+	if (strstr(class, "Steam") || strstr(class, "steam_app_"))
+		c->issteam = 1;
 
 	for (i = 0; i < LENGTH(rules); i++) {
 		r = &rules[i];
@@ -640,13 +647,15 @@ configurerequest(XEvent *e)
 			c->bw = ev->border_width;
 		else if (c->isfloating || !selmon->lt[selmon->sellt]->arrange) {
 			m = c->mon;
-			if (ev->value_mask & CWX) {
-				c->oldx = c->x;
-				c->x = m->mx + ev->x;
-			}
-			if (ev->value_mask & CWY) {
-				c->oldy = c->y;
-				c->y = m->my + ev->y;
+			if (!c->issteam) {
+				if (ev->value_mask & CWX) {
+					c->oldx = c->x;
+					c->x = m->mx + ev->x;
+				}
+				if (ev->value_mask & CWY) {
+					c->oldy = c->y;
+					c->y = m->my + ev->y;
+				}
 			}
 			if (ev->value_mask & CWWidth) {
 				c->oldw = c->w;
@@ -1634,6 +1643,9 @@ setfocus(Client *c)
 		XChangeProperty(dpy, root, netatom[NetActiveWindow],
 			XA_WINDOW, 32, PropModeReplace,
 			(unsigned char *) &(c->win), 1);
+	}
+	if (c->issteam) {
+		setclientstate(c, NormalState);
 	}
 	sendevent(c, wmatom[WMTakeFocus]);
 }
